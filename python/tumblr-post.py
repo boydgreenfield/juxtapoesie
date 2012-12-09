09859202d677ec754ce6e40c911d0069f4f54df0
Boyd Greenfield <boyd.greenfield@gmail.com>
2012-12-08 21:53:22 -0800

    Add "click to enlarge" title to images

--- python/tumblr-post.py
+++ python/tumblr-post.py
@@ -123,11 +123,11 @@ for j in Juxtas:
     body2 = ("<div class=\"juxta\">" +
             "<div class=\"left-image\">" +
             "<a title=\"&lt;a target=&quot;_blank&quot; href=&quot;" + j.url1 + "&quot; &gt;Link to the original image&lt;/a&gt;\" rel=\"lightbox\" href=\"" + j.url1 + "\">" +
-            "<img src=\"" + url1az + "\" width=" + str(IMAGE_SIZE[0]) +
+            "<img title=\"Click to enlarge\"src=\"" + url1az + "\" width=" + str(IMAGE_SIZE[0]) +
             " />" + "</a>" + "</div>" +
             "<div class=\"right-image\">" +
             "<a title=\"&lt;a target=&quot;_blank&quot; href=&quot;" + j.url2 + "&quot; &gt;Link to the original image&lt;/a&gt;\" rel=\"lightbox\" href=\"" + j.url2 + "\">" +
-            "<img src=\"" + url2az + "\" width=" + str(IMAGE_SIZE[0]) +
+            "<img title=\"Click to enlarge\" src=\"" + url2az + "\" width=" + str(IMAGE_SIZE[0]) +
             " />" + "</a>" + "</div>" +
             "<p class=\"top-info\">" + "Left: " + j.leftcredit + " / " +
                     " Right: " + j.rightcredit + " / " +
